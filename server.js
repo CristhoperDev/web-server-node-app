3120d4acfec2c496ac79652f25356889311ef9c7
Cristhoper Alvarez <acristhoper25@gmail.com>
2020-04-25 21:52:57 -0500

    using helpers

--- server.js
+++ server.js
@@ -1,24 +1,22 @@
 const express = require('express')
 const app = express()
-const hbs = require('hbs');
 
+app.use(express.static( __dirname + '/public' ));
+
+const hbs = require('hbs');
+require('./hbs/helpers');
 //Express HBS Engine
 hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
 
-app.use(express.static( __dirname + '/public' ));
-
 app.get('/', (req, res) => {
     res.render('home', {
-        name: 'Cristhoper',
-        year: new Date().getFullYear()
+        name: 'cristhoper alvarez',
     });
 });
 
 app.get('/about', (req, res) => {
-    res.render('about', {
-        year: new Date().getFullYear()
-    });
+    res.render('about');
 });
 
 app.listen(3000, () => {
