4948174a9b8df4c259ff27dfa14b395cddb1bf33
Cristhoper Alvarez <acristhoper25@gmail.com>
2020-04-25 21:41:51 -0500

    partials hbs

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const app = express()
+const hbs = require('hbs');
 
 //Express HBS Engine
+hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
 
 app.use(express.static( __dirname + '/public' ));
@@ -9,6 +11,12 @@ app.use(express.static( __dirname + '/public' ));
 app.get('/', (req, res) => {
     res.render('home', {
         name: 'Cristhoper',
+        year: new Date().getFullYear()
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about', {
         year: new Date().getFullYear()
     });
 });
