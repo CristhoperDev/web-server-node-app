f6e8be01359c8567da7a0e1068be2d61b669686b
Cristhoper Alvarez <acristhoper25@gmail.com>
2020-04-25 22:03:14 -0500

    deploy heroku

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const app = express()
 
-app.use(express.static( __dirname + '/public' ));
-
 const hbs = require('hbs');
 require('./hbs/helpers');
+
+const port = process.env.PORT || 3000;
+
+app.use(express.static( __dirname + '/public' ));
 //Express HBS Engine
 hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
@@ -19,6 +21,6 @@ app.get('/about', (req, res) => {
     res.render('about');
 });
 
-app.listen(3000, () => {
-    console.log(`Listen Port: 3000`);
+app.listen(port, () => {
+    console.log(`Listen Port: ${port}`);
 });
